6166024c3de53b1d41e0cedaafffaba47d36cb01
firminm <matthewjf1@gmail.com>
2022-10-13 13:18:21 -0400

    Changed how parameters are accepted

--- azure/HttpTrigger1/__init__.py
+++ azure/HttpTrigger1/__init__.py
@@ -1,10 +1,9 @@
 import logging
-import uuid
 import azure.functions as func
 
 
 """
-curl -X POST -H "Content-type: application/json" -d "{\"email\" : \"testuser@testing.com\", \"password\" : \"12345\"}" "http://assessment.519.buspark.io:8000/auth/register"
+curl -X POST -H "Content-type: application/json" -d "{\"name\" : \"Lemons\", \"password\" : \"azure\", \"id\" : \"12\", \"recieved\" : \"1\", \"shipped\" : \"2\", \"returned\" : \"1\', \"onHan\" : \"23\"}" "https://famicy.azurewebsites.net/api/item/create"
 """
 def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -17,12 +16,13 @@ def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpRespon
     num_returned = req.route_params.get('returned')
     num_on_hand = req.route_params.get('onHand')
 
+    print(password,name,id,num_recieved,num_shipped,num_returned,num_on_hand)
 
-    print(name, id)
     if not name:
         try:
             req_body = req.get_json()
         except ValueError:
+            print(req)
             pass
         else:
             password = req_body.get('password')
@@ -32,6 +32,8 @@ def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpRespon
             num_shipped = req_body.get('shipped')
             num_returned = req_body.get('returned')
             num_on_hand = req_body.get('onHand')
+        
+    print(password, name, id, num_recieved, num_shipped, num_returned, num_on_hand)
 
     if password != 'azure':
         return func.HttpResponse("You do not have access to this resource", status_code=403)
@@ -49,7 +51,7 @@ def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpRespon
         newdocs.append(func.Document.from_dict(newproduct_dict))
         doc.set(newdocs)
         
-        return func.HttpResponse(f"Item successfully processed!", 200)
+        return func.HttpResponse(f"Item successfully processed!", status_code=200)
     else:
         return func.HttpResponse(
             "Missing parameters, please make sure you have provided the following fields: name, id, recieved, shipped, returned, onHand", 
