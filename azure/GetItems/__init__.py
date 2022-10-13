f281cd30d850888bc3e0b2b13698948c555b13d2
firminm <matthewjf1@gmail.com>
2022-10-13 13:17:35 -0400

    added password protection

--- azure/GetItems/__init__.py
+++ azure/GetItems/__init__.py
@@ -5,6 +5,10 @@ import azure.functions as func
 def main(req: func.HttpRequest, doc:func.DocumentList) -> func.HttpResponse:
     
     logging.info('Python HTTP trigger function processed a request.')
+
+    password = req.route_params.get('password')
+    if password != 'azure':
+        return func.HttpResponse("You do not have access to this resource", status_code=403)
  
     items_json = []
     for item in doc:
